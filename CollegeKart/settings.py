d8cdfefe31e6e8946ab2775d2a6312d45b6844b4
rohith1618 <mude.rohith.cse21@itbhu.ac.in>
2022-11-13 11:41:54 +0530

    commmit1 	new file:   CollegeKart/__pycache__/__init__.cpython-39.pyc 	new file:   CollegeKart/__pycache__/settings.cpython-39.pyc 	new file:   CollegeKart/__pycache__/urls.cpython-39.pyc 	new file:   CollegeKart/__pycache__/wsgi.cpython-39.pyc 	modified:   CollegeKart/settings.py 	new file:   accounts/__pycache__/__init__.cpython-39.pyc 	new file:   accounts/__pycache__/admin.cpython-39.pyc 	new file:   accounts/__pycache__/apps.cpython-39.pyc 	new file:   accounts/__pycache__/models.cpython-39.pyc 	new file:   accounts/__pycache__/urls.cpython-39.pyc 	new file:   accounts/__pycache__/views.cpython-39.pyc 	new file:   accounts/migrations/__pycache__/__init__.cpython-39.pyc 	new file:   cart/__pycache__/__init__.cpython-39.pyc 	new file:   cart/__pycache__/admin.cpython-39.pyc 	new file:   cart/__pycache__/apps.cpython-39.pyc 	new file:   cart/__pycache__/models.cpython-39.pyc 	new file:   cart/migrations/__pycache__/0001_initial.cpython-39.pyc 	new file:   cart/migrations/__pycache__/__init__.cpython-39.pyc 	new file:   home/__pycache__/__init__.cpython-39.pyc 	new file:   home/__pycache__/admin.cpython-39.pyc 	new file:   home/__pycache__/apps.cpython-39.pyc 	new file:   home/__pycache__/forms.cpython-39.pyc 	new file:   home/__pycache__/models.cpython-39.pyc 	new file:   home/__pycache__/urls.cpython-39.pyc 	new file:   home/__pycache__/views.cpython-39.pyc 	new file:   home/migrations/__pycache__/0001_initial.cpython-39.pyc 	new file:   home/migrations/__pycache__/0002_contact_slug.cpython-39.pyc 	new file:   home/migrations/__pycache__/0003_auto_20201121_1058.cpython-39.pyc 	new file:   home/migrations/__pycache__/0004_contact_created.cpython-39.pyc 	new file:   home/migrations/__pycache__/0005_remove_contact_created.cpython-39.pyc 	new file:   home/migrations/__pycache__/__init__.cpython-39.pyc 	new file:   product/__pycache__/__init__.cpython-39.pyc 	new file:   product/__pycache__/admin.cpython-39.pyc 	new file:   product/__pycache__/apps.cpython-39.pyc 	new file:   product/__pycache__/models.cpython-39.pyc 	new file:   product/migrations/__pycache__/0001_initial.cpython-39.pyc 	new file:   product/migrations/__pycache__/0002_auto_20201031_2044.cpython-39.pyc 	new file:   product/migrations/__pycache__/0003_delete_productimages.cpython-39.pyc 	new file:   product/migrations/__pycache__/__init__.cpython-39.pyc

--- CollegeKart/settings.py
+++ CollegeKart/settings.py
@@ -83,8 +83,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'collegekart',
-        'USER': 'CollegeKart',
-        'PASSWORD': 'CollegeKart%123',
+        'USER': 'root',
+        'PASSWORD': 'Rohith@2004',
         'HOST': '127.0.0.1',                   
         'PORT': '3306',  
     }
